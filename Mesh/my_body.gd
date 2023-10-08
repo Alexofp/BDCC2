acc47ba96b57a752b72e2804e2db093eb27b4d88
Rahi <vip-ak47@yandex.ru>
2023-10-08 14:20:11 +0300

    Some blendshapes, better rigging
    Former-commit-id: eb2b8904dd06f4f203180600b35d98c2e57330c1
    Former-commit-id: 7835b38b81ade71c3e1042205c94e15d65f59f7e

---
+++ Mesh/my_body.gd
@@ -0,0 +1,41 @@
+extends Node3D
+
+var savedOptions = {}
+
+func _ready():
+	resetOptionsToDefault()
+	#applyOption("thickbutt", 1.0)
+
+func _process(delta):
+	pass
+
+func resetOptionsToDefault():
+	var theOptions = getOptions()
+	
+	for optionKey in theOptions:
+		savedOptions[optionKey] = theOptions[optionKey]["default"]
+
+func getOptions() -> Dictionary:
+	return {
+		"breastsize": {
+			"type": "slider",
+			"minvalue": 0.0,
+			"maxvalue": 1.0,
+			"default": 0.5,
+		},
+		"thickbutt": {
+			"type": "slider",
+			"minvalue": 0.0,
+			"maxvalue": 1.0,
+			"default": 0.0,
+		},
+	}
+
+func setBlendshape(mesh: MeshInstance3D, blendShapeID: String, val: float):
+	var blendShapeIndex = mesh.find_blend_shape_by_name(blendShapeID)
+	mesh.set_blend_shape_value(blendShapeIndex, val)
+
+func applyOption(optionID, value):
+	if(optionID == "thickbutt"):
+		setBlendshape($rig/Skeleton3D/Body, "ThickButt", value)
+		
