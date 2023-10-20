82ca7d34e9ce2eafd5529d2ebbf110433e9fe9e8
Rahi <vip-ak47@yandex.ru>
2023-10-20 22:09:04 +0300

    Height slider fixed hopefully
    Former-commit-id: c3c077d516026c8c38187a72b160d6faf30d04d1
    Former-commit-id: 84bcaa2952d6d98173e206206527920f31d9f5be

--- Player/Doll/doll.gd
+++ Player/Doll/doll.gd
@@ -27,12 +27,12 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("move_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
