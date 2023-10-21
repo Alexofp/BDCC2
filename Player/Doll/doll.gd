d01e2f716b464e5dac137e810db710bcf8f740be
Rahi <vip-ak47@yandex.ru>
2023-10-21 12:22:02 +0300

    Better character controller

--- Player/Doll/doll.gd
+++ Player/Doll/doll.gd
@@ -1,11 +1,5 @@
-extends CharacterBody3D
+extends Node3D
 class_name Doll
-
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 var character:BaseCharacter
 #var dollSkeleton:DollSkeleton
@@ -20,28 +14,6 @@ func setCharacter(newChar:BaseCharacter):
 	# Add support for bodyparts being removed
 	newChar.connect("onBodypartAdded", onBodypartChanged)
 	newChar.connect("onBodypartRemoved", onBodypartRemoved)
-
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("move_jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
 
 func clear():
 	pass
@@ -126,3 +98,6 @@ func updateBodypartRecursive(parentPart:BaseBodypart, slot:String, part:BaseBody
 			var childPart:BaseBodypart = childParts[bodypartSlot]
 			
 			updateBodypartRecursive(part, bodypartSlot, childPart)
+
+func getDoll() -> Doll:
+	return self
