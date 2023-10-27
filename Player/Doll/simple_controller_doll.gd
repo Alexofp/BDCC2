791c5e5dbaa7548d52d013c80ddbdaaa5293a12c
Rahi <vip-ak47@yandex.ru>
2023-10-27 21:44:22 +0300

    Better way to handle UI
    Former-commit-id: a2692e13031c513b2cc3082c12b8f7d936e0cff2
    Former-commit-id: df7cf419ad77930425eb134c65b02fab78f9c5ff

--- Player/Doll/simple_controller_doll.gd
+++ Player/Doll/simple_controller_doll.gd
@@ -1,11 +1,15 @@
 extends CharacterBody3D
 
+var startPos:Vector3 = Vector3.ZERO
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	startPos = position
 
 func getDoll() -> Doll:
 	return $Doll.getDoll()
@@ -14,6 +18,9 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+
+	if(position.y < -100.0):
+		position = startPos
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("move_jump") and is_on_floor():
