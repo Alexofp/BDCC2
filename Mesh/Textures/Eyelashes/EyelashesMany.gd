525e20829d738d68a7b41249a41bf96619fb0dac
Rahi <vip-ak47@yandex.ru>
2024-02-06 13:37:14 +0300

    More head tweaks
    Former-commit-id: 7b3608da6c4576e7ed4eba4ff487d7a0539ae9d9
    Former-commit-id: 7e71e74dcb061f05a42876d5e466f07b73407861

--- Mesh/Textures/Eyelashes/EyelashesMany.gd
+++ Mesh/Textures/Eyelashes/EyelashesMany.gd
@@ -11,4 +11,6 @@ func _init():
 		{id="eyelash3", name="Eyelash 3", path="res://Mesh/Textures/Eyelashes/eyelash3.png"},
 		{id="eyelash4", name="Eyelash 4", path="res://Mesh/Textures/Eyelashes/eyelash4.png"},
 		{id="eyelash5", name="Eyelash 5", path="res://Mesh/Textures/Eyelashes/eyelash5.png"},
+		{id="eyelash6", name="Eyelash 6", path="res://Mesh/Textures/Eyelashes/eyelash6.png"},
+		{id="eyelash7", name="Eyelash 7", path="res://Mesh/Textures/Eyelashes/eyelash7.png"},
 	]
