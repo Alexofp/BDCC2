65934d06925f896083100cec42a5a6c3bdf76171
Rahi <vip-ak47@yandex.ru>
2025-09-19 08:41:28 +0300

    Ear skins

--- Mesh/Parts/Ear/FluffyEar/Textures/Patterns/FluffyEarManyPatterns.gd
+++ Mesh/Parts/Ear/FluffyEar/Textures/Patterns/FluffyEarManyPatterns.gd
@@ -15,9 +15,54 @@ func _init():
 				hasB=false,
 			},
 		},
+		"Gradient": {
+			name = "Gradient",
+			colormask = "res://Mesh/Parts/Ear/FluffyEar/Textures/Patterns/Gradient.png",
+			flags = {
+				hasR=true,
+				hasG=true,
+				hasB=true,
+			},
+		},
 		"Dots": {
 			name = "Dots",
 			colormask = "res://Mesh/Parts/Ear/FluffyEar/Textures/Patterns/Dots.png",
+			flags = {
+				hasR=true,
+				hasG=true,
+				hasB=true,
+			},
+		},
+		"WeirdMarkings": {
+			name = "Weird markings",
+			colormask = "res://Mesh/Parts/Ear/FluffyEar/Textures/Patterns/WeirdMarkings.png",
+			flags = {
+				hasR=true,
+				hasG=true,
+				hasB=true,
+			},
+		},
+		"Square": {
+			name = "Square",
+			colormask = "res://Mesh/Parts/Ear/FluffyEar/Textures/Patterns/Square.png",
+			flags = {
+				hasR=true,
+				hasG=true,
+				hasB=true,
+			},
+		},
+		"Geometric": {
+			name = "Geometric",
+			colormask = "res://Mesh/Parts/Ear/FluffyEar/Textures/Patterns/Geometric.png",
+			flags = {
+				hasR=true,
+				hasG=true,
+				hasB=true,
+			},
+		},
+		"Tip": {
+			name = "Tip",
+			colormask = "res://Mesh/Parts/Ear/FluffyEar/Textures/Patterns/Tip.png",
 			flags = {
 				hasR=true,
 				hasG=true,
